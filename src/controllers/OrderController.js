e163bccd49c1ee3dc9a249eee96849ab7c7ec266
Luciano <54756923+lucianovictor@users.noreply.github.com>
2022-04-06 16:39:41 -0300

    :recycle: update ordercontroller

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,19 +1,43 @@
-const mongoose = require('mongoose');
+const Order = require('../models/Order');
 
-const OrderSchema = new mongoose.Schema({
-  table: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ['PENDING', 'PREPARING', 'DONE'],
-    default: 'PENDING',
-  },
-});
+class OrderController {
+  async index(req, res) {
+    const orders = await Order.find();
+    res.json(orders);
+  }
 
-module.exports = mongoose.model('Order', OrderSchema);
+  async store(req, res) {
+    const { table, description } = req.body;
+
+    if (!table || !description) {
+      return res.sendStatus(400);
+    }
+
+    const order = await Order.create({
+      table, description,
+    });
+
+    req.io.emit('newOrder', order);
+    res.json(order);
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    if (!status) {
+      return res.sendStatus(400);
+    }
+
+    const order = await Order.findByIdAndUpdate(
+      { _id: id },
+      { status },
+      { new: true },
+    );
+
+    req.io.emit('statusChange', order);
+    res.json(order);
+  }
+}
+
+module.exports = new OrderController();
